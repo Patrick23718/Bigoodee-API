cad339c04aae825c38ba694909baf75b0fd14f8e
Patrick23718 <32969033+Patrick23718@users.noreply.github.com>
2022-06-24 05:35:44 +0100

    Update db.config.js

--- api/config/db.config.js
+++ api/config/db.config.js
@@ -2,5 +2,5 @@ module.exports = {
   HOST: "localhost",
   PORT: 27017,
   DB: "Bigoodee",
-  URL: "mongodb+srv://test:test@cluster0.htrmr.mongodb.net/Bigoodee?retryWrites=true&w=majority",
+  URL: "mongodb+srv://test:test@cluster0.htrmr.mongodb.net/Bigoodees?retryWrites=true&w=majority",
 };
